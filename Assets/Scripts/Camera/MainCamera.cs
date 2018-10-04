56f7b6f9e5560a8e798d02649ff3813ac232c4f2
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-10-04 10:46:12 +0200

    Camera script & Document pick up

---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    private Transform player;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        transform.position = new Vector2(player.position.x + 5.5f, 0);
+    }
+}
