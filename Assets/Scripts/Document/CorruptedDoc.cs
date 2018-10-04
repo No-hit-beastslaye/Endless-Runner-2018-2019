82263f0f2f12c4ddd604780dcd8019fb74b69acc
Brandon Ruigrok <31778356+No-hit-beastslaye@users.noreply.github.com>
2018-10-04 11:37:22 +0200

    Corrupted document script added.
    Has working death trigger

---
+++ Assets/Scripts/Document/CorruptedDoc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorruptedDoc : MonoBehaviour
+{
+    public Component player;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Deadend();
+        }
+    }
+
+    public void Deadend()
+    {
+        print("DIE");
+        player.GetComponent<PlayerMovement>().SetStop(true);
+    }
+}
